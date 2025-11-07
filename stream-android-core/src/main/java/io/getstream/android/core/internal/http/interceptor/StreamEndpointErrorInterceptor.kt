807a181a50143d4fd5c34748f7f66920e1081d0d
Gian <47775302+gpunto@users.noreply.github.com>
2025-11-07 10:28:51 +0100

    Apply Spotless with new configuration

--- stream-android-core/src/main/java/io/getstream/android/core/internal/http/interceptor/StreamEndpointErrorInterceptor.kt
+++ stream-android-core/src/main/java/io/getstream/android/core/internal/http/interceptor/StreamEndpointErrorInterceptor.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.getstream.android.core.internal.http.interceptor
 
 import io.getstream.android.core.api.model.exceptions.StreamEndpointException
--- stream-android-core/src/test/java/io/getstream/android/core/internal/client/StreamClientIImplTest.kt
+++ stream-android-core/src/test/java/io/getstream/android/core/internal/client/StreamClientIImplTest.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 @file:OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
 
 package io.getstream.android.core.internal.client
