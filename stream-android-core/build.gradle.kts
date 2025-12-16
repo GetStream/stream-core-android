db8e7e02a1d4b4d8d754ab1192a98b46744cb01f
Gianmarco <47775302+gpunto@users.noreply.github.com>
2025-12-16 10:46:45 +0100

    Configure publishing through convention plugins and migrate to new workflow (#40)

--- stream-android-core/build.gradle.kts
+++ stream-android-core/build.gradle.kts
@@ -1,7 +1,5 @@
 @file:OptIn(ExperimentalAbiValidation::class)
 
-import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
-import io.getstream.core.Configuration
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
 
@@ -10,7 +8,6 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
     alias(libs.plugins.arturbosch.detekt)
-    alias(libs.plugins.maven.publish)
 }
 
 kotlin {
@@ -85,19 +82,4 @@ dependencies {
     testImplementation(libs.mockk)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.mockwebserver)
-}
-
-mavenPublishing {
-    coordinates(
-        groupId = Configuration.artifactGroup,
-        artifactId = "stream-android-core",
-        version = rootProject.version.toString(),
-    )
-    configure(
-        AndroidSingleVariantLibrary(
-            variant = "release",
-            sourcesJar = true,
-            publishJavadocJar = true,
-        ),
-    )
 }
